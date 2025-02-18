f30de57f8e32421266e545f1e53a38b928b45a9e
Chetan Mittal <chetanm.it.22@nitj.ac.in>
2025-02-19 01:04:13 +0530

    Initial Commit

---
+++ test1Tests/test1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  test1Tests.swift
+//  test1Tests
+//
+//  Created by Chetan Mittal on 2025/02/19.
+//
+
+import Testing
+@testable import test1
+
+struct test1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
